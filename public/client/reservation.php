638a70de9151feb773d9a02f1f016cd249107812
Hamza GBOURI <hamzagbouri2004@gmail.com>
2024-12-26 11:42:43 +0100

    fixed reservation page acces

--- public/client/reservation.php
+++ public/client/reservation.php
@@ -2,10 +2,12 @@
 
 require_once ('../../app/helpers/getAllReservations.php');
 require_once ('../../app/helpers/getActivite.php');
+session_start();
 if(!isset($_SESSION['id']))
 {
-    header('Location: /GDV-v2/public/client/home.php');
+    header('Location: /GV2/public/client/home.php');
 }
+
 $reservations = getAllReservations::getAllReservations();
 $allActivites = getActivite::getAllActivites();
 ?>
@@ -78,7 +80,7 @@ $allActivites = getActivite::getAllActivites();
 </head>
 <body class="w-full h-full min-h-screen text-white font-primary ">
     <?php include 'header.php'; 
-     
+   
         
     ?>
     <section class="bg-bgcolor py-16 px-8 text-black">
